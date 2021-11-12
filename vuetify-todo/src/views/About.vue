30eddfd1ff25d18b8af6095b254d7bec5d3b4d04
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-12 21:55:25 +0300

    create todo app

---
+++ vuetify-todo/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nesciunt voluptatum hic aliquid dolores omnis dicta quidem blanditiis sequi obcaecati, ea cumque placeat debitis commodi ullam, labore libero porro magnam.</p>
+  </div>
+</template>
